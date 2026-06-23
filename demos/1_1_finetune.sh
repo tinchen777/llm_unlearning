8d6bcf923ed1cf1530b915d9c574a2d4b2bbe3eb
tinchen777 <59043279+tinchen777@users.noreply.github.com>
2026-06-23 13:08:12 +0800

    new: add new fintune.sh

--- demos/1_1_finetune.sh
+++ demos/1_1_finetune.sh
@@ -28,6 +28,7 @@ python src/train.py --config-name=train.yaml \
   experiment=finetune/tofu/default \
   model=${MODEL} \
   trainer.args.eval_on_start=False \
+  trainer.args.num_train_epochs=20 \
   task_name=test/tofu_${MODEL}_full \
 
 # 产物: saves/finetune/tofu_${MODEL}_full
